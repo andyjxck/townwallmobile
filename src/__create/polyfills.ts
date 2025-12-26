215616e583d7da9b3967d6c7edf36098ed5804ed
Andrew Blewett <andysocial@MacBookAir.lan>
2025-12-26 21:14:35 +0000

    Auto-sync after agent response

--- src/__create/polyfills.ts
+++ src/__create/polyfills.ts
@@ -1,3 +1,12 @@
 import updatedFetch from './fetch';
+import * as Crypto from 'expo-crypto';
+import bcrypt from 'bcryptjs';
+
 // @ts-ignore
 global.fetch = updatedFetch;
+
+// Polyfill for bcryptjs random fallback in React Native
+bcrypt.setRandomFallback((len) => {
+  const array = Crypto.getRandomValues(new Uint8Array(len));
+  return Array.from(array);
+});
