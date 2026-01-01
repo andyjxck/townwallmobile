021f70f2573b2f70498f79d31dbe2283be18b5a5
andyjxck <your@email.com>
2026-01-01 23:59:39 +0000

    Auto-sync after agent response

--- plugins/withAgoraFix.js
+++ plugins/withAgoraFix.js
@@ -26,13 +26,28 @@ const withAgoraFix = (config) => {
       const oldPreInstallPattern = /pre_install do \|installer\|[\s\S]*?# Fix Agora aosl\.xcframework conflict[\s\S]*?end\n/g;
       podfileContent = podfileContent.replace(oldPreInstallPattern, '');
 
+      // Improved Agora Fix block with physical deletion as recommended by Agora
       const agoraPreInstallBlock = `
   pre_install do |installer|
     # Fix Agora aosl.xcframework conflict between AgoraRtm and AgoraInfra_iOS
     installer.pod_targets.each do |pod|
       if pod.name == 'AgoraRtm' || pod.name == 'AgoraRtmKit'
         puts "Fixing Agora conflict for #{pod.name}: removing aosl.xcframework"
-        pod.spec_consumer.instance_variable_get(:@spec).attributes_hash['vendored_frameworks'].reject! { |f| f.include?('aosl.xcframework') } rescue nil
+        
+        # 1. Remove from spec to prevent CocoaPods from linking it twice
+        begin
+          pod.spec_consumer.instance_variable_get(:@spec).attributes_hash['vendored_frameworks'].reject! { |f| f.include?('aosl.xcframework') }
+        rescue => e
+          puts "Could not remove from spec: #{e.message}"
+        end
+
+        # 2. Physically remove the framework folder as recommended by official Agora docs
+        rtm_pod_path = File.join(installer.sandbox.root, pod.name)
+        aosl_xcframework_path = File.join(rtm_pod_path, 'aosl.xcframework')
+        if File.exist?(aosl_xcframework_path)
+          puts "Deleting aosl.xcframework from #{aosl_xcframework_path}"
+          FileUtils.rm_rf(aosl_xcframework_path)
+        end
       end
     end
   end
@@ -40,7 +55,6 @@ const withAgoraFix = (config) => {
 
       // Find a good place to insert the pre_install block
       // We want it before the target 'TownWall' do ... end block ends, or at the top.
-      // Usually after platform :ios, ... is a good spot.
       if (!podfileContent.includes('pre_install do |installer|')) {
         podfileContent = podfileContent.replace(
           /target 'TownWall' do/,
@@ -54,10 +68,28 @@ const withAgoraFix = (config) => {
     installer.pod_targets.each do |pod|
       if pod.name == 'AgoraRtm' || pod.name == 'AgoraRtmKit'
         puts "Fixing Agora conflict for #{pod.name}: removing aosl.xcframework"
-        pod.spec_consumer.instance_variable_get(:@spec).attributes_hash['vendored_frameworks'].reject! { |f| f.include?('aosl.xcframework') } rescue nil
+        
+        # 1. Remove from spec
+        begin
+          pod.spec_consumer.instance_variable_get(:@spec).attributes_hash['vendored_frameworks'].reject! { |f| f.include?('aosl.xcframework') }
+        rescue
+        end
+
+        # 2. Physically remove
+        rtm_pod_path = File.join(installer.sandbox.root, pod.name)
+        aosl_xcframework_path = File.join(rtm_pod_path, 'aosl.xcframework')
+        if File.exist?(aosl_xcframework_path)
+          puts "Deleting aosl.xcframework from #{aosl_xcframework_path}"
+          FileUtils.rm_rf(aosl_xcframework_path)
+        end
       end
     end`
         );
+      }
+
+      // Ensure FileUtils is required at the top of Podfile if not present
+      if (!podfileContent.includes("require 'fileutils'")) {
+        podfileContent = "require 'fileutils'\n" + podfileContent;
       }
 
       fs.writeFileSync(podfilePath, podfileContent);
